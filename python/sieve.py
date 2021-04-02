78f7400e81ced23c64c1e1258de26025eec5fa80
jnguyen1098 <jnguyen1098@gmail.com>
2021-04-02 16:00:25 -0400

    python impl, but need to optimize it. its very c-like rn

---
+++ python/sieve.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import time
+import sieve_assert
+
+NUM_LIMIT = 100000
+NUM_PRIMES = 9592
+
+def sieve(results):
+    n = 0
+    numbers = [0] * (NUM_LIMIT + 1)
+    for i in range(2, NUM_LIMIT + 1):
+        for j in range(2 * i, NUM_LIMIT + 1, i):
+            numbers[j] = 1
+    for i in range(2, NUM_LIMIT + 1):
+        if numbers[i] == 0:
+            results[n] = i
+            n += 1
+
+results = [0] * NUM_PRIMES
+
+start = time.time()
+sieve(results)
+end = time.time()
+
+"""
+for i in range(len(sieve_assert.expected_results)):
+    if results[i] != sieve_assert.expected_results[i]:
+        print(f"Expected {sieve_assert.expected_results[i]} but got {results[i]}")
+"""
+
+print(f"{end - start}")
